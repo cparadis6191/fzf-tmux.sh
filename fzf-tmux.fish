dbcf60abe3ee99c7a773fb6c9f81a6bd10ce501a
Chad Paradis <cparadis6191@gmail.com>
2024-12-21 12:08:27 -0500

    Join strings in fish key bindings

--- fzf-tmux.fish
+++ fzf-tmux.fish
@@ -1,2 +1,2 @@
-bind \cx\cl 'commandline --insert (_fzf_tmux_capture_pane_lines)'
-bind \cx\cf 'commandline --insert (_fzf_tmux_capture_pane_words)'
+bind \cx\cl 'commandline --insert (_fzf_tmux_capture_pane_lines | string join " ")'
+bind \cx\cf 'commandline --insert (_fzf_tmux_capture_pane_words | string join " ")'
