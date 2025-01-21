9dc8d0feb71cf6beadd265c9744ea32d2ccc944a
Chad Paradis <cparadis6191@gmail.com>
2025-01-20 20:25:24 -0500

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ fzf, stdenv, tmux, ... }:
+
+stdenv.mkDerivation {
+  pname = "fzf-tmux";
+  version = "0.0.1";
+
+  src = ./.;
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir --parents $out/bin/
+
+    install -D ./bin/__fzf_tmux_fzf               $out/bin/
+    install -D ./bin/_fzf_tmux_capture_pane_lines $out/bin/
+    install -D ./bin/_fzf_tmux_capture_pane_words $out/bin/
+
+    mkdir --parents $out/share/fzf-tmux/
+
+    install -D ./fzf-tmux.fish $out/share/fzf-tmux/fzf-tmux.fish
+
+    runHook postInstall
+  '';
+
+  outputs = [ "out" ];
+}
