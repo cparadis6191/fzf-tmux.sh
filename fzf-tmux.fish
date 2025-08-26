d99305a6c197e4f626a94336ee3b4f39bd872d40
Chad Paradis <cparadis6191@gmail.com>
2025-08-25 20:54:46 -0400

    Add fish key bindings without control modifier

--- fzf-tmux.fish
+++ fzf-tmux.fish
@@ -4,3 +4,6 @@ end
 
 bind \cx\cl '__fzf_tmux_fish _fzf_tmux_capture_pane_lines'
 bind \cx\cf '__fzf_tmux_fish _fzf_tmux_capture_pane_words'
+
+bind \cxl '__fzf_tmux_fish _fzf_tmux_capture_pane_lines'
+bind \cxf '__fzf_tmux_fish _fzf_tmux_capture_pane_words'
