4320f65af7fef3b170390c634eaac154f6a250fd
Chad Paradis <cparadis6191@gmail.com>
2025-09-06 13:59:16 -0400

    Remove __fzf_tmux_fzf/_fzf_tmux_capture_pane_lines/_fzf_tmux_capture_pane_words
    Remove `__fzf_tmux_fzf`, `_fzf_tmux_capture_pane_lines`, and
    `_fzf_tmux_capture_pane_words`. Remove `__fzf_tmux_fzf`,
    `_fzf_tmux_capture_pane_lines`, and `_fzf_tmux_capture_pane_words` from
    default.nix.

--- default.nix
+++ default.nix
@@ -13,12 +13,6 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
 
-    mkdir --parents $out/bin/
-
-    install -D ./bin/__fzf_tmux_fzf               $out/bin/
-    install -D ./bin/_fzf_tmux_capture_pane_lines $out/bin/
-    install -D ./bin/_fzf_tmux_capture_pane_words $out/bin/
-
     mkdir --parents $out/share/fzf-tmux/
 
     install -D ./fzf-tmux.fish    $out/share/fzf-tmux/
