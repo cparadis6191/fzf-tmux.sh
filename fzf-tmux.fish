fb085f44473db6af22147fcc9348906a901a1fa0
Chad Paradis <cparadis6191@gmail.com>
2025-09-06 13:53:39 -0400

    Rework fzf-tmux.fish to use tmux

--- fzf-tmux.fish
+++ fzf-tmux.fish
@@ -1,19 +1,19 @@
-function __fzf_git_sh
+function __fzf_tmux_sh
     # Get the absolute path to the parent directory of this script (i.e. the
-    # parent directory of fzf-git.sh) to use in the key bindings to avoid
+    # parent directory of fzf-tmux.sh) to use in the key bindings to avoid
     # having to modify `$PATH`.
-    set --function fzf_git_sh_path (realpath (status dirname))
+    set --function fzf_tmux_sh_path (realpath (status dirname))
 
-    commandline --insert (SHELL=bash bash "$fzf_git_sh_path/fzf-git.sh" --run $argv | string join ' ')
+    commandline --insert (SHELL=bash bash "$fzf_tmux_sh_path/fzf-tmux.sh" --run $argv | string join ' ')
 end
 
-set --local commands branches each_ref files hashes lreflogs remotes stashes tags worktrees
+set --local commands fcapture_pane_words lcapture_pane_lines
 
 for command in $commands
     set --function key (string sub --length=1 $command)
 
-    eval "bind -M default \cg$key   '__fzf_git_sh $command'"
-    eval "bind -M insert  \cg$key   '__fzf_git_sh $command'"
-    eval "bind -M default \cg\c$key '__fzf_git_sh $command'"
-    eval "bind -M insert  \cg\c$key '__fzf_git_sh $command'"
+    eval "bind -M default \cx$key   '__fzf_tmux_sh $command'"
+    eval "bind -M insert  \cx$key   '__fzf_tmux_sh $command'"
+    eval "bind -M default \cx\c$key '__fzf_tmux_sh $command'"
+    eval "bind -M insert  \cx\c$key '__fzf_tmux_sh $command'"
 end
